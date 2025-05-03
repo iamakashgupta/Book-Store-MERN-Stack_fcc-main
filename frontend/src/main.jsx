47bcffb8461951e9b67d3c95a0a2d84c4b1f712a
iamakashgupta <akashg6980@gmail.com>
2025-05-03 23:08:54 +0530

    Background sticked is added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <SnackbarProvider>
+      <div class="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div class="bg-no-repeat fixed bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+      </div>
       <App />
     </SnackbarProvider>
   </BrowserRouter>
 );
-
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
